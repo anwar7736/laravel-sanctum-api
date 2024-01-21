1933c13ea3c6d9a6204ce5cf69bdc4eb55839695
Md Anwar Hossain <78712716+anwar7736@users.noreply.github.com>
2024-01-21 19:10:05 +0600

    Add files via upload

---
+++ app/ModelActionCauser.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Traits;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
+
+trait ModelActionCauser
+{
+    public function __construct()
+    {
+        $causer_id = Auth::id() ?? 1;
+        static::creating(function ($model) use($causer_id){
+                $tableName = $model->getTable();
+                $createdByColumnExists = Schema::hasColumn($tableName, 'created_by');
+                if ($createdByColumnExists) 
+                {
+                    $model->created_by = $causer_id;
+                }
+            
+        });
+
+        static::updating(function ($model) use($causer_id){
+                $tableName = $model->getTable();
+                $updatedByColumnExists = Schema::hasColumn($tableName, 'updated_by');
+                if ($updatedByColumnExists) 
+                {
+                    $model->updated_by = $causer_id;
+                }
+            
+        });
+    }
+}
